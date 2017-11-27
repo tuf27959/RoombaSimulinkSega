a08526d292e814745af30a3f1dd444192c4d9fc8
tuf27959 <31491676+tuf27959@users.noreply.github.com>
2017-11-26 21:53:56 -0500

    Update Sensor.py

--- Project 3/ROS_SCRIPTS/Sensor.py
+++ Project 3/ROS_SCRIPTS/Sensor.py
@@ -1,1 +1,31 @@
+#!/usr/bin/env python
 
+from __future__ import print_function
+from pycreate2 import Create2
+import time
+
+import rospy
+from std_msgs.msg import Int32MultiArray
+
+rospy.init_node('IR_publisher')
+
+pub = rospy.Publisher('IR_sensor',Int32MultiArray)
+
+rate = rospy.Rate(2)
+
+port = '/dev/ttyUSB0'
+bot = Create2(port=port, baud=115200)
+
+bot.start()
+bot.full()
+print('Starting ...')
+
+while not rospy.is_shutdown():
+	sensor_state = bot.get_sensors()
+
+	time.sleep(0.1)
+	x = sensor_state[35]
+	y = (int(x[0]),int(x[1]),int(x[2]),int(x[3]),int(x[4]),int(x[5]),)
+	z = Int32MultiArray(data=y)
+	pub.publish(z)
+	rate.sleep()
